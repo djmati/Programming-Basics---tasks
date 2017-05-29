946ed928ef5c880147d5739431a3d2e52fe114df
Mati <djmati.bg@gmail.com>
2017-05-29 17:05:47 +0300

    ChangeSource
    Смяна на кода

--- 01Distance.cs
+++ 01Distance.cs
@@ -1,38 +1,66 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CheckPrime
+namespace Hotel
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            bool istesting = true;
-            while (istesting)
+            var month = Console.ReadLine();
+            var nights = int.Parse(Console.ReadLine());
+            var studioPrice = 0.00;
+            var doublePrice = 0.00;
+            var suitePrice = 0.00;
+
+            switch (month)
             {
-                Console.Write("Enter even number: ");
-                string number = Console.ReadLine();
+                case "May":
+                case "October":
+                    studioPrice = 50;
+                    doublePrice = 65;
+                    suitePrice = 75;
 
-                try
-                {
-                    int numeral = Convert.ToInt32(number);
-                    if (numeral % 2 == 0 && numeral != 0)
+                    if (nights > 7)
                     {
-                        Console.WriteLine("Even number entered: {0}", numeral);
-                        istesting = false;
+                        studioPrice *= 0.95;
                     }
+                    break;
+                case "June":
+                case "September":
+                    studioPrice = 60;
+                    doublePrice = 72;
+                    suitePrice = 82;
 
-                    else
-                        Console.WriteLine("The number is not even.");
-                }
-                catch
-                {
-                    Console.WriteLine("Невалиден номер!");
-                }
+                    if (nights >  14)
+                    {
+                        doublePrice *= 0.90;
+                    }
+                    break;
+                case "July":
+                case "August":
+                case "December":
+                    studioPrice = 68;
+                    doublePrice = 77;
+                    suitePrice = 89;
+
+                    if (nights > 14)
+                    {
+                        suitePrice *= 0.85;
+                    }
+                    break;
             }
+
+            var totalStudioPrice = studioPrice * nights;
+            var totalDoublePrice = doublePrice * nights;
+            var totalSuitePrice = suitePrice * nights;
+
+            if ((month == "September" || month == "October") && nights > 7)
+            {
+                totalStudioPrice -= studioPrice;
+            }
+            Console.WriteLine($"Studio: {totalStudioPrice:F2} lv.");
+            Console.WriteLine($"Double: {totalDoublePrice:F2} lv.");
+            Console.WriteLine($"Suite: {totalSuitePrice:F2} lv.");
         }
     }
 }
